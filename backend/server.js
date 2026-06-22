e30c3a948c7fdcc561df383e9479e65262c846a2
Siddhi Naik <siddhisnaik04@gmail.com>
2026-06-22 22:05:15 +0530

    server.js

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server Running...");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
