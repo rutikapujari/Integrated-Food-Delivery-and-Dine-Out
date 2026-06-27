80f92cc4e1970e129749b1a83e818e08ab93fa0f
Siddhi Naik <naik261004@gmail.com>
2026-06-27 22:37:40 +0530

    new

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,61 +1,143 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
+
     name: {
         type: String,
-        required: [true, 'Name is required'],
+        required: [true, "Name is required"],
         trim: true
     },
+
     email: {
         type: String,
-        required: [true, 'Email is required'],
+        required: [true, "Email is required"],
         unique: true,
         lowercase: true
     },
+
     password: {
         type: String,
-        required: [true, 'Password is required'],
+        required: [true, "Password is required"],
         minlength: 6,
-        select: false        // never returned in queries by default
+        select: false
     },
+
     role: {
         type: String,
-        enum: ['customer', 'restaurant', 'courier', 'admin'],
-        default: 'customer'
+        enum: [
+            "customer",
+            "restaurant",
+            "courier",
+            "admin"
+        ],
+        default: "customer"
     },
+
     loyaltyPoints: {
         type: Number,
-        default: 0           // earned by placing orders + writing reviews
+        default: 0
     },
+
     address: {
         type: String,
-        default: ''
+        default: ""
     },
+
     location: {
+
         type: {
             type: String,
-            enum: ['Point'],
-            default: 'Point'
+            enum: ["Point"],
+            default: "Point"
         },
+
         coordinates: {
-            type: [Number],    // [longitude, latitude]
+            type: [Number],
             default: [0, 0]
         }
+
+    },
+
+
+    // EMAIL VERIFICATION
+
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    emailToken: {
+        type: String,
+        default: null
+    },
+
+
+    // FORGOT PASSWORD
+
+    resetToken: {
+        type: String,
+        default: null
+    },
+
+    resetExpire: {
+        type: Date,
+        default: null
     }
-}, { timestamps: true });
 
-userSchema.index({ location: '2dsphere' });  // needed for geospatial queries
+},
 
-// Hash password before saving
-userSchema.pre('save', async function () {
-    if (!this.isModified('password')) return;
-    this.password = await bcrypt.hash(this.password, 12);
+    {
+        timestamps: true
+    });
+
+
+// GEO INDEX
+
+userSchema.index({
+    location: "2dsphere"
 });
 
-// Compare entered password with hashed
-userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-};
 
-module.exports = mongoose.model('User', userSchema);
+// HASH PASSWORD
+
+userSchema.pre(
+    "save",
+    async function () {
+
+        if (
+            !this.isModified(
+                "password"
+            )
+        ) return;
+
+        this.password =
+            await bcrypt.hash(
+                this.password,
+                12
+            );
+
+    }
+);
+
+
+// MATCH PASSWORD
+
+userSchema.methods.matchPassword =
+    async function (
+        enteredPassword
+    ) {
+
+        return await bcrypt.compare(
+            enteredPassword,
+            this.password
+        );
+
+    };
+
+
+module.exports =
+    mongoose.model(
+        "User",
+        userSchema
+    );
